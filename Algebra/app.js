84b424849a9c8bcf1da7941aacccd205b52054da
Weslley <samuraiweslleyjack@gmail.com>
2020-06-26 04:27:59 -0300

    Update app.js

--- Algebra/app.js
+++ Algebra/app.js
@@ -6,3 +6,11 @@ function night(){
             c.className='night';
             body.className='night';
         }
+
+/* Dark Mode */
+function setModeEventListener() {
+  let list = document.body.classList;
+  document.getElementById("toggler").addEventListener("change", event => {
+    event.target.checked ? list.add("dark-mode") : list.remove("dark-mode");
+  });
+}
